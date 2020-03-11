a001134d53e17454125d517498bc251bca66567f
Lee Saenz <leesaenz@gmail.com>
2020-03-10 21:08:11 -0600

    Finish

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 require('dotenv').config();
-const { CommentStream } = require("snoostorm");
 
 require('dotenv').config();
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
 
 const r = new Snoowrap({
-    userAgent: 'mikethebot',
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    username: process.env.REDDIT_USER,
-    password: process.env.REDDIT_PASS,
-    automatic_retries: true,
-    api_requests_per_minute: 1
+  userAgent: 'mikethebot',
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS,
+  automatic_retries: false,
+  api_requests_per_minute: 60,
+  refreshToken: process.env.REFRESH_TOKEN,
+  accessToken: process.env.ACCESS_TOKEN
 });
 
-r.getTop('all', { time: 'week', limit: 10000 }).map(post => {
-    r.getSubreddit(process.env.SUBREDDIT).addContributor({ name: post.author.name });
+function delay(t, data) {
+  return new Promise(resolve => {
+    setTimeout(resolve.bind(null, data), t);
+  });
 }
-).then(console.log('done'));
+
+function addUser(sub, user) {
+  r.getSubreddit(sub).addContributor({ name: user });
+  return console.log('added ' + user);
+}
+
+function listUsernames(user) {
+  return console.log('returning ' + user);
+}
+
+topList = r.getTop('all', {
+  time: 'today',
+  limit: 50
+});
+
+topList
+  .map(post => {
+    addUser(process.env.SUBREDDIT, post.author.name);
+    //listUsernames(post.author.name);
+  })
+  .then(console.log('running'));
